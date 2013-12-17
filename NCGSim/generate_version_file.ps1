510266aed7398028a54d974e7b361ea8cfcc6db8
Fabsenet <fabsenet@googlemail.com>
2013-12-17 08:56:58 +0100

    added powershell script to build a version file on every build
    the version is buildnumber-branchname-shorthash if the branch is not master
    or buildnumber-shorhash for master

---
+++ NCGSim/generate_version_file.ps1
@@ -0,0 +1,12 @@
+﻿param ([string]$targetfile)
+
+$buildcount = git rev-list --count HEAD
+$lasthash = git rev-list --max-count=1 --abbrev-commit HEAD
+$branchname = git rev-parse --abbrev-ref HEAD
+
+if($branchname -eq 'master'){
+    [void][System.IO.File]::WriteAllText($targetfile, $buildcount + "-" + $lasthash)
+}
+else{
+    [void][System.IO.File]::WriteAllText($targetfile, $buildcount + "-" + $branchname+ "-" + $lasthash)
+}
