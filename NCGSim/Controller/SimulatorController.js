3404bab6ed63dcf7255290b0d7cf6454ef061387
Fabian Wetzel <fabsenet@googlemail.com>
2014-01-28 20:02:17 +0100

    added a simulator.html as a starting point with referenced angular, bootstrap and all models.
    added nuget packages as needed

---
+++ NCGSim/Controller/SimulatorController.js
@@ -0,0 +1,7 @@
+/// <reference path="../model/_references.ts" />
+angular.module('simulatorController', ['ui.bootstrap']);
+var ButtonsCtrl = function ($scope) {
+    var state = new State();
+    $scope.sometext = state.calculatePartialConnectionCosts.toString();
+};
+//# sourceMappingURL=SimulatorController.js.map
---
+++ NCGSim/Controller/SimulatorController.ts
@@ -0,0 +1,8 @@
+/// <reference path="../model/_references.ts" />
+
+
+angular.module('simulatorController', ['ui.bootstrap']);
+var ButtonsCtrl = function ($scope) {
+    var state = new State();
+    $scope.sometext = state.calculatePartialConnectionCosts.toString();
+}; 
