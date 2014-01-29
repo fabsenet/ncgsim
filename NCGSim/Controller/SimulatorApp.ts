75d66fd06a0d54859bee660638d5a4b17620acf1
Fabian Wetzel <fabsenet@googlemail.com>
2014-01-29 21:56:38 +0100

    reworked controller to use a proper viewmodel

--- NCGSim/Controller/SimulatorApp.ts
+++ NCGSim/Controller/SimulatorApp.ts
@@ -1,33 +1,48 @@
 /// <reference path="../model/_references.ts" />
 
+class StateViewModel {
+    nodes: INode<NodeData>[];
+    addNode = function () {
+        this.state.graph.addNode(new NodeData(Math.round(Math.random() * 19) + 1, Math.round(Math.random() * 19) + 1));
+        this.$scope.refresh();
+    };
+    removeNode = function () {
+        var nodes = this.state.graph.getNodes();
+        if (nodes.length == 0) return;
+        this.state.graph.removeNode(_.last(nodes));
+        this.$scope.refresh();
+    };
+
+    refresh = function () {
+        this.$scope.vm.nodes = this.state.graph.getNodes();
+    };
+
+    private $scope: stateCtrlScope;
+    private state:State;
+    constructor(scope: stateCtrlScope, state:State) {
+        this.$scope = scope;
+        this.state = state;
+
+        this.refresh();
+    }
+}
+
+interface stateCtrlScope extends ng.IScope {
+    state: State;
+    vm: StateViewModel;
+    refresh: () => void;
+}
+
 angular.module('SimulatorApp', ['ui.bootstrap']);
-var stateCtrl = function($scope:ng.IScope) {
+var stateCtrl = function($scope: stateCtrlScope) {
     var state = new State();
     state.graph.addNode(new NodeData(1, 1));
     state.graph.addNode(new NodeData(8, 2));
     state.graph.addNode(new NodeData(3, 7));
 
     $scope.state = state;
+    $scope.vm = new StateViewModel($scope, state);
 
-    $scope.vm = {};
-
-    $scope.refresh =() => {
-        $scope.vm.nodes = state.graph.getNodes();
-    };
-
-    $scope.refresh();
-
-    $scope.vm.addNode = function() {
-        state.graph.addNode(new NodeData(Math.round(Math.random() * 19)+1, Math.round(Math.random() * 19)+1));
-        $scope.refresh();
-    };
-
-    $scope.vm.removeNode = function () {
-        var nodes = state.graph.getNodes();
-        if (nodes.length == 0) return;
-        state.graph.removeNode(_.last(nodes));
-        $scope.refresh();
-    };
 
     $scope.costs = {
         calculatePartialConnectionCosts: state.calculatePartialConnectionCosts.toString(),
@@ -37,12 +52,4 @@ var stateCtrl = function($scope:ng.IScope) {
     $scope.settings = {
         gameManipulation: state.gameSettings.gameManipulation == null ? "null" : state.gameSettings.gameManipulation.toString()
     };
-
-    
-    $scope.vm.readableOperationMode = state.gameSettings.operationMode == OperationMode.SEQUENTIAL ? "SEQUENTIAL" : "PARALLEL";
-
-    $scope.vm.refreshReadableOperationMode = function(selectedMode: OperationMode) {
-        $scope.vm.readableOperationMode = selectedMode == OperationMode.SEQUENTIAL ? "SEQUENTIAL" : "PARALLEL";
-    };
-
-}; 
+};
