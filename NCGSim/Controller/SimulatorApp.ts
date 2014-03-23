34b5d2320b76b7e38d97b5b6948848b96a5df74e
Fabian Wetzel <fabsenet@googlemail.com>
2014-03-23 21:42:09 +0100

    added ability to add and remove edges in the simulation setup (still a bit rough UI experience)

--- NCGSim/Controller/SimulatorApp.ts
+++ NCGSim/Controller/SimulatorApp.ts
@@ -2,6 +2,11 @@
 
 angular.module('SimulatorApp', ['ui.bootstrap', 'app.directives.graphviz', 'app.directives.ncgversion']);
 
+interface IEdge {
+    startNode: INode<NodeData>;
+    endNode: INode<NodeData>;
+}
+
 class stateCtrl {
 
     constructor($scope) {
@@ -22,11 +27,12 @@ class stateCtrl {
         };
 
         this.refresh();
+        $scope.$watch("vm", this.refresh.bind(this), true);
     }
 
     state: State;
     nodes: INode<NodeData>[];
-
+    edges: IEdge[];
 
     costs: {
         calculatePartialConnectionCosts: string;
@@ -45,20 +51,36 @@ class stateCtrl {
         }
     }
 
-    addNode() {
+    addNode():void {
         this.state.graph.addNode(new NodeData(Math.round(Math.random() * 300) + 1, Math.round(Math.random() * 300) + 1));
         this.refresh();
     }
 
-    removeNode() {
+    removeNode():void {
         var nodes = this.state.graph.getNodes();
         if (nodes.length == 0) return;
         this.state.graph.removeNode(_.last(nodes));
         this.refresh();
     }
 
-    refresh() {
+    private refresh(): void {
         this.nodes = this.state.graph.getNodes();
+        this.edges = this.getEdges();
     }
 
+    private getEdges(): IEdge[] {
+        var edges: IEdge[]=[];
+
+        _.forEach(this.state.graph.getNodes(), (startNode:INode<NodeData>) => {
+            _.forEach(startNode.connectedEdges, (endNodeId: number)=> {
+                var endNode = this.state.graph.getNodeById(endNodeId);
+                edges.push({
+                    startNode: startNode,
+                    endNode: endNode
+                });
+            });
+        });
+
+        return edges;
+    }
 }
