8bba085b548137e5276688352531cd06fa4273e3
Fabian Wetzel <fabsenet@googlemail.com>
2014-07-21 22:57:35 +0200

    Made predefined graphs work if the app runs in a subfolder

--- NCGSim/Controller/PredefinedGraphsController.ts
+++ NCGSim/Controller/PredefinedGraphsController.ts
@@ -14,14 +14,14 @@ class PredefinedGraphsController {
         console.log("scope", $scope.vm);
         $scope.samplesvm = this;
 
-        $http({ method: 'GET', url: '/Model/Samples/_list.json' })
+        $http({ method: 'GET', url: 'Model/Samples/_list.json' })
             .success((obj) => {
                 console.log("received sampleList!", obj);
                 this.sampleList = <ISampleListItem[]>obj;
 
                 _.forEach(this.sampleList, (item:ISampleListItem) => {
 
-                    $http({ method: 'GET', url: '/Model/Samples/' + item.url,transformResponse : (stateString)=> { return JSON.parse(stateString, State.reviver); }})
+                    $http({ method: 'GET', url: 'Model/Samples/' + item.url,transformResponse : (stateString)=> { return JSON.parse(stateString, State.reviver); }})
                         .success((graph) => {
                             console.log("received graph for item!", graph, item);
                             item.state = graph;
